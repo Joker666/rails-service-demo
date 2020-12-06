e467d8d7ca840066a206c32ac86fcd174bac6620
Hasan Rafi <mr.k779@outlook.com>
2020-12-07 04:50:49 +0600

    Rails service demo read me added

--- README.md
+++ README.md
@@ -1,24 +1,60 @@
-# README
+# Ruby on Rails Patterns: Service Objects and Interactors
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+![logo](https://res.cloudinary.com/practicaldev/image/fetch/s--jvDLhx0b--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/cpcr5w0kgl6j94tss7n9.png)
 
-Things you may want to cover:
+## About The Project
+This is a Ruby on Rails demo application that shows how to organise a fat controller intro service objects.
+And then goes further into adding `interactor` gem and showing how to better organise this concept
+of service objects with interactors.
 
-* Ruby version
+## How To Run
+This is a Ruby on Rails API only application. Since we only need API to demonstrate the patterns. Let's create the app with
 
-* System dependencies
+```bash
+rails new sentry-rails \
+  --skip-action-text \
+  --skip-active-storage \
+  --skip-javascript \
+  --skip-spring -T \
+  --skip-turbolinks \
+  --skip-sprockets \
+  --skip-test \
+  --api
+```
 
-* Configuration
+We will skip some of the parts that we know are not necessary for this demonstration. The migration and seed are already in. So let's run
 
-* Database creation
+```bash
+rails db:migrate db:seed
+```
 
-* Database initialization
+The controllers are versioned into v1, v2, v3. Each controller shows how to tackle the problem with user registration.
+This controller handles user creation, subscription creation for user, adding a dedicated support person for the user, sending welcome email and update time series based metrics table for revenue calculation.
+You can hit the api with
 
-* How to run the test suite
+```bash
+curl -XPOST -H "Content-type: application/json" -d '{
+    "name": "Blake Dawson",
+    "email": "blake@dawson.com",
+    "pass": "OnePass",
+    "product_name": "Business"
+}' 'localhost:3000/api/v1/user/create'
+```
 
-* Services (job queues, cache servers, search engines, etc.)
+Replace v1 with v2 or v3 to hit different versions of same controller
 
-* Deployment instructions
+## Contribution
+Want to contribute? Great!
 
-* ...
+To fix a bug or enhance an existing code, follow these steps:
+
+- Fork the repo
+- Create a new branch (`git checkout -b improve-feature`)
+- Make the appropriate changes in the files
+- Add changes to reflect the changes made
+- Commit your changes (`git commit -am 'Improve feature'`)
+- Push to the branch (`git push origin improve-feature`)
+- Create a Pull Request
+
+## License
+MIT © [MD Ahad Hasan](https://github.com/joker666)
--- app/controllers/v1/user_controller.rb
+++ app/controllers/v1/user_controller.rb
@@ -1,40 +1,36 @@
 module V1
     class UserController < ApplicationController
         def create
-            ActiveRecord::Base.transaction do
-                # Create User
-                prev_user = User.find_by email: params[:email]
-                if prev_user.present?
-                    render json: { error: 'User already exists' }
-                    return
+            begin
+                ActiveRecord::Base.transaction do
+                    # Create User
+                    prev_user = User.find_by email: params[:email]
+                    raise StandardError, 'User already exists' if prev_user.present?
+
+                    user = User.create(name: params[:name], email: params[:email], pass: params[:pass])
+
+                    # Create Subscription
+                    product = Product.find_by name: params[:product_name]
+                    raise StandardError, "Product doesn't exist" unless product.present?
+
+                    sub = Subscription.create(product_id: product[:id], user_id: user[:id], expires_at: Time.now + 30.day)
+
+                    # Assign support person
+                    support = Support.find_by name: 'Jessica'
+                    raise StandardError, "Couldn't assign a support person"  unless support.present?
+
+                    user.support_id = support[:id]
+                    user.save
+
+                    # Update Metrics
+                    Metric.create(user_count: 1, revenue: product.price)
+
+                    # Send welcome email
+                    UserMailer.with(user: user).welcome_email.deliver_later
+                    render json: { user: user, subscription: sub }
                 end
-
-                user = User.create(name: params[:name], email: params[:email], pass: params[:pass])
-
-                # Create Subscription
-                product = Product.find_by name: params[:product_name]
-                unless product.present?
-                    render json: { error: "Product doesn't exist" }
-                    return
-                end
-
-                sub = Subscription.create(product_id: product[:id], user_id: user[:id], expires_at: Time.now + 30.day)
-
-                # Assign support person
-                support = Support.find_by name: 'Jessica'
-                unless support.present?
-                    render json: { error: "Couldn't assign a support person" }
-                    return
-                end
-                user.support_id = support[:id]
-                user.save
-
-                # Update Metrics
-                Metric.create(user_count: 1, revenue: product.price)
-
-                # Send welcome email
-                UserMailer.with(user: user).welcome_email.deliver_later
-                render json: { user: user, subscription: sub }
+            rescue StandardError => e
+                render json: { error: e }
             end
         end
     end
