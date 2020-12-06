7da09feefa29b737b88aa24afabd7731eac40620
Hasan Rafi <mr.k779@outlook.com>
2020-12-07 05:11:54 +0600

    Fix indent

--- app/controllers/v1/user_controller.rb
+++ app/controllers/v1/user_controller.rb
@@ -1,38 +1,37 @@
 module V1
-    class UserController < ApplicationController
-        def create
-            begin
-                ActiveRecord::Base.transaction do
-                    # Create User
-                    prev_user = User.find_by email: params[:email]
-                    raise StandardError, 'User already exists' if prev_user.present?
+  class UserController < ApplicationController
+    def create
+      begin
+        ActiveRecord::Base.transaction do 
+          # Create User
+          prev_user = User.find_by email: params[:email]
+          raise StandardError, 'User already exists' if prev_user.present?
 
-                    user = User.create(name: params[:name], email: params[:email], pass: params[:pass])
+          user = User.create(name: params[:name], email: params[:email], pass: params[:pass])
 
-                    # Create Subscription
-                    product = Product.find_by name: params[:product_name]
-                    raise StandardError, "Product doesn't exist" unless product.present?
+          # Create Subscription
+          product = Product.find_by name: params[:product_name]
+          raise StandardError, "Product doesn't exist" unless product.present?
 
-                    sub = Subscription.create(product_id: product[:id], user_id: user[:id], expires_at: Time.now + 30.day)
+          sub = Subscription.create(product_id: product[:id], user_id: user[:id], expires_at: Time.now + 30.day)
 
-                    # Assign support person
-                    support = Support.find_by name: 'Jessica'
-                    raise StandardError, "Couldn't assign a support person"  unless support.present?
+          # Assign support person
+          support = Support.find_by name: 'Jessica'
+          raise StandardError, "Couldn't assign a support person"  unless support.present?
 
-                    user.support_id = support[:id]
-                    user.save
+          user.support_id = support[:id]
+          user.save
 
-                    # Update Metrics
-                    Metric.create(user_count: 1, revenue: product.price)
+          # Update Metrics
+          Metric.create(user_count: 1, revenue: product.price)
 
-                    # Send welcome email
-                    UserMailer.with(user: user).welcome_email.deliver_later
-                    render json: { user: user, subscription: sub }
-                end
-            rescue StandardError => e
-                render json: { error: e }
-            end
+          # Send welcome email
+          UserMailer.with(user: user).welcome_email.deliver_later
+          render json: { user: user, subscription: sub }
         end
+      rescue StandardError => e
+        render json: { error: e }
+      end
     end
+  end
 end
-
--- app/controllers/v2/user_controller.rb
+++ app/controllers/v2/user_controller.rb
@@ -1,25 +1,25 @@
 module V2
-    class UserController < ApplicationController
-        def create
-            begin
-                ActiveRecord::Base.transaction do
-                    # Create User
-                    user = CreateUserService.call(params[:name], params[:email], params[:pass])
-                    # Get Product
-                    product = GetProductService.call(params[:product_name])
-                    # Create Subscription
-                    sub = CreateSubscriptionService.call(product[:id], user[:id])
-                    # Assign support person
-                    AssignSupportService.call(user, 'Jessica')
-                    # Update Metrics
-                    UpdateMetricsService.call(product)
-                    # Send welcome email
-                    WelcomeEmailService.call(user)
-                    render json: { user: user, subscription: sub }
-                end
-            rescue StandardError => e
-                render json: { error: e }
-            end
+  class UserController < ApplicationController
+    def create
+      begin
+        ActiveRecord::Base.transaction do
+          # Create User
+          user = CreateUserService.call(params[:name], params[:email], params[:pass])
+          # Get Product
+          product = GetProductService.call(params[:product_name])
+          # Create Subscription
+          sub = CreateSubscriptionService.call(product[:id], user[:id])
+          # Assign support person
+          AssignSupportService.call(user, 'Jessica')
+          # Update Metrics
+          UpdateMetricsService.call(product)
+          # Send welcome email
+          WelcomeEmailService.call(user)
+          render json: { user: user, subscription: sub }
         end
+      rescue StandardError => e
+        render json: { error: e }
+      end
     end
+  end
 end
